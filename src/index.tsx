53ff1427a4427258e7f56b73e24d81f80367069c
Tuan Thanh Bui <bttuan@cmcglobal.vn>
2022-04-10 15:21:14 +0700

    config firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,27 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import GlobalCss from "./styles/global.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8WBAJN6PHdCNlLDNANtrP7xTcJIXBJTM",
+  authDomain: "hasco-demo.firebaseapp.com",
+  projectId: "hasco-demo",
+  storageBucket: "hasco-demo.appspot.com",
+  messagingSenderId: "586153137602",
+  appId: "1:586153137602:web:2d756482731daf87b0325b",
+  measurementId: "G-54X39BHF2G",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
